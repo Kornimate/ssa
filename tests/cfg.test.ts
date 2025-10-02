a1d50f3b5972acc015dc830e0cef6154c0e50968
Kornidesz Máté <mko@rushfiles.com>
2025-10-02 14:09:46 +0200

    unnecessary parts removed

--- tests/cfg.test.ts
+++ tests/cfg.test.ts
@@ -1,10 +1,10 @@
 //look at jelly for test material, tests/micro folder
 
 import { describe, it, expect } from 'vitest';
-import { test } from '../src/dummy';
+//imports
 
 describe('test function', () => {
   it('adds two numbers correctly', () => {
-    expect(test(2, 3)).toBe(5);
+    expect(true).toBe(true);
   });
 });
